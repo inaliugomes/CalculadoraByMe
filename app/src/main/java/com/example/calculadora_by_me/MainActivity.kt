e405a103733ad230614db92de31069352d678271
Inaliu Gomes <inaliugomes@gmail.com>
2023-12-05 16:58:29 +0000

    Quase com botões finalizados , inicializando na logica

--- app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
@@ -2,22 +2,51 @@ package com.example.calculadora_by_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.calculadora_by_me.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
-    lateinit var bindig : ActivityMainBinding
+    lateinit var binding : ActivityMainBinding
+
     var total: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindig = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindig.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button0.setOnClickListener(this)
+        binding.button1.setOnClickListener(this)
+        binding.button2.setOnClickListener(this)
+        binding.button3.setOnClickListener(this)
+        binding.button4.setOnClickListener(this)
+        binding.button5.setOnClickListener(this)
+        binding.button6.setOnClickListener(this)
+        binding.button7.setOnClickListener(this)
+        binding.button8.setOnClickListener(this)
+        binding.button9.setOnClickListener(this)
+        binding.buttonAC.setOnClickListener(this)
+        binding.buttonDivide.setOnClickListener(this)
+        binding.buttonParentese.setOnClickListener(this)
+        binding.buttonPlus.setOnClickListener(this)
+        binding.buttonSubtration.setOnClickListener(this)
+        binding.buttonDot.setOnClickListener(this)
 
 
     }
 
 
+
+
+
+
+    override fun onClick(view: View) {
+
+        if(view.id == R.id.button_0){
+
+        }
+    }
 
 
 
