7bd8aeb28b7dae44489a9f208c22edd518ebfc93
Inaliu Gomes <inaliugomes@gmail.com>
2023-12-05 00:02:16 +0000

    With Button

---
+++ app/src/main/java/com/example/calculadora_by_me/Calculadora.kt
@@ -0,0 +1,27 @@
+package com.example.calculadora_by_me
+
+class Calculadora {
+
+
+    fun somar(){
+
+    }
+
+    fun multiplicar(){
+
+    }
+
+    fun subtrair(){
+
+    }
+
+    fun dividir(){
+
+    }
+
+
+}
+
+fun main() {
+
+}
--- app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.calculadora_by_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculadora_by_me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindig : ActivityMainBinding
+    var total: Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindig = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindig.root)
+
+
     }
+
+
+
+
+
+    fun calcular(b : Int) : Int{
+
+        if(1-2 ==2 ) {
+
+            total = total - b
+
+        } else if(-2 ==2  ){
+
+            total = total + b
+        }else if(-2 ==2  ){
+
+            total = total * b
+        }else if(-2 ==2  ){
+
+            total = total / b
+        }
+
+
+        return total
+    }
+
+
 }
