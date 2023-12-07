38b8d56f8d052f660794b18c50772486353a549b
Inaliu Gomes <inaliugomes@gmail.com>
2023-12-07 21:49:07 +0000

    Logica da aplicação limpa para tentar novamente

--- app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
@@ -8,9 +8,11 @@ import com.example.calculadora_by_me.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var binding: ActivityMainBinding
+
     var total: Int = 0;
-    var numero: Int = 0;
+    var valorTempString : String = ""
     var valorEmString: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //Use to hide the ActionBar
         supportActionBar?.hide()
 
+        //Lisiners
         binding.button0.setOnClickListener(this)
         binding.button1.setOnClickListener(this)
         binding.button2.setOnClickListener(this)
@@ -40,57 +43,61 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.textInfo.setOnClickListener(this)
         binding.buttonDelete.setOnClickListener(this)
         binding.buttonX.setOnClickListener(this)
+        binding.textResult.setOnClickListener(this)
+        binding.buttonIqual.setOnClickListener(this)
 
 
     }
+
+
 
 
     override fun onClick(view: View) {
 
         if (view.id == R.id.button_0) {
-            valorString(0)
+
         } else if (view.id == R.id.button_1) {
-            valorString(1)
+
         } else if (view.id == R.id.button_2) {
-            valorString(2)
+
         } else if (view.id == R.id.button_3) {
-            valorString(3)
+
         } else if (view.id == R.id.button_4) {
-            valorString(4)
+
         } else if (view.id == R.id.button_5) {
-            valorString(5)
+
         } else if (view.id == R.id.button_6) {
-            valorString(6)
+
         } else if (view.id == R.id.button_7) {
-            valorString(7)
+
         } else if (view.id == R.id.button_8) {
-            valorString(8)
+
         } else if (view.id == R.id.button_9) {
-            valorString(9)
+
         }else if(view.id == R.id.button_AC){
 
-            valorEmString = ""
-            binding.textInfo.text = ""
+
         } else if (view.id == R.id.button_delete){
-           valorEmString = valorEmString.dropLast(1)
-            binding.textInfo.text =  valorEmString
+
+
+
+
         }else if (view.id == R.id.button_plus){
 
-            valorEmString = valorEmString.plus("+")
-            binding.textInfo.text = valorEmString
+
+
         }else if (view.id == R.id.button_subtration){
 
-            valorEmString = valorEmString.plus("-")
-            binding.textInfo.text = valorEmString
 
         }else if (view.id == R.id.button_divide){
 
-            valorEmString = valorEmString.plus("÷")
-            binding.textInfo.text = valorEmString
+
+
         }else if (view.id == R.id.button_x){
 
-            valorEmString = valorEmString.plus("×")
-            binding.textInfo.text = valorEmString
+
+        }else if(view.id == R.id.button_iqual){
+
         }
 
 
@@ -98,34 +105,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
-    fun valorString(n: Int) {
-        val num = n
-        valorEmString = valorEmString.plus(num.toString())
-        binding.textInfo.text = valorEmString
-
-    }
 
 
-    fun calcular(b: Int): Int {
-
-        if (1 - 2 == 2) {
-
-            total = total - b
-
-        } else if (-2 == 2) {
-
-            total = total + b
-        } else if (-2 == 2) {
-
-            total = total * b
-        } else if (-2 == 2) {
-
-            total = total / b
-        }
-
-
-        return total
-    }
 
 
 }
