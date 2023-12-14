fd86b1a2a5a12e5bc502f85fc762753f43f8af46
Inaliu Gomes <inaliugomes@gmail.com>
2023-12-14 12:21:34 +0000

    Calculadora a funcionar com calculos basicos de dois funões ,Softwere a melhorar quando obter mais conhecimento e mais tempo para trabalhar nas melhorias , buscar por outros.

--- app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_by_me/MainActivity.kt
@@ -11,10 +11,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityMainBinding
 
 
-    var valorStringInput : String = ""
-    var valorDisplay : String = ""
+    var valorStringInput: String = ""
+    var valorDisplay: String = ""
     var lista = mutableListOf<Any>()
-    var total : Int = 0
+    var total: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
@@ -51,115 +51,132 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
-
-
     override fun onClick(view: View) {
 
         if (view.id == R.id.button_0) {
+            receber(0)
 
         } else if (view.id == R.id.button_1) {
-           receber(1)
+            receber(1)
         } else if (view.id == R.id.button_2) {
             receber(2)
         } else if (view.id == R.id.button_3) {
+            receber(3)
 
         } else if (view.id == R.id.button_4) {
-
+            receber(4)
         } else if (view.id == R.id.button_5) {
-
+            receber(5)
         } else if (view.id == R.id.button_6) {
-
+            receber(6)
         } else if (view.id == R.id.button_7) {
-
+            receber(7)
         } else if (view.id == R.id.button_8) {
-
+            receber(8)
         } else if (view.id == R.id.button_9) {
+            receber(9)
+        } else if (view.id == R.id.button_AC) {
 
-        }else if(view.id == R.id.button_AC){
+            deleteAll()
+        } else if (view.id == R.id.button_delete) {
+           // deleteLast()
+            
+        } else if (view.id == R.id.button_plus) {
 
-
-        } else if (view.id == R.id.button_delete){
-
-
-        }else if (view.id == R.id.button_plus){
-
-            lista.add(valorStringInput.toInt())
-            lista.add("+")
-            valorDisplay = valorDisplay.plus("+")
-            binding.textInfo.text = valorDisplay
-            valorStringInput = ""
+           sinal("+")
 
 
-        }else if (view.id == R.id.button_subtration){
+        } else if (view.id == R.id.button_subtration) {
 
+            sinal("-")
 
-        }else if (view.id == R.id.button_divide){
+        } else if (view.id == R.id.button_divide) {
 
+            sinal("÷")
 
+        } else if (view.id == R.id.button_x) {
+            sinal("*")
 
-        }else if (view.id == R.id.button_x){
+        } else if (view.id == R.id.button_iqual) {
 
+            if (!valorStringInput.isEmpty()){
+                lista.add(valorStringInput.toInt())
+            }
 
-        }else if(view.id == R.id.button_iqual){
-
-         lista.add(valorStringInput.toInt())
-
-         calculadorExpressao(lista)
-
+            calculadorExpressao(lista)
 
 
         }
 
 
-
     }
 
-    fun receber(number : Int ){
+    fun sinal(sinal :String){
+
+        lista.add(valorStringInput.toInt())
+        lista.add(sinal)
+        valorDisplay = valorDisplay.plus(sinal)
+        binding.textInfo.text = valorDisplay
+        valorStringInput = ""
+    }
+
+    fun receber(number: Int) {
 
         //Tranformação do numero em String
         valorStringInput = valorStringInput.plus(number.toString())
-       valorDisplay = valorDisplay.plus(number.toString())
+        valorDisplay = valorDisplay.plus(number.toString())
         binding.textInfo.text = valorDisplay
 
     }
 
-    fun calculadorExpressao(vararg elementos: Any): Int {
+    fun calculadorExpressao(elementos: MutableList<Any>): Int {
 
         //Modolo de 2 para garantir que existe pelo menos dois numero dentro do objeto
-        if (elementos.isEmpty() || elementos.size % 2 == 0){
-            throw java.lang.IllegalArgumentException("Expressão Invalida")
-        }
+        if (elementos.isEmpty() || elementos.size % 2 == 0) {
+            total = 0
+        }else {
+            val primeiroElemento = elementos[0]
 
-        //Atribuição do primeiro valor para nosso resultado
-        val primeiroElemento = elementos[0]
+            if (primeiroElemento is Integer) {
 
-        if(primeiroElemento is Integer){
-
-            total = primeiroElemento.toInt()
-        }
-
-        for( i in 1 until elementos.size step 2 ){
-
-            val operação = elementos[i] as String
-            val numero:Int = elementos[i +1 ] as Int
-
-            total = when(operação){
-                "+" -> total + numero
-                "-" -> total - numero
-                "*" -> total * numero
-                "/" -> total / numero
-                else -> throw java.lang.IllegalArgumentException("Operação Inválida:")
+                total = primeiroElemento.toInt()
             }
+
+            for (i in 1 until elementos.size step 2) {
+
+                val operação = elementos[i] as String
+                val numero: Int = elementos[i + 1] as Int
+
+                total = when (operação) {
+                    "+" -> total + numero
+                    "-" -> total - numero
+                    "*" -> total * numero
+                    "÷" -> total / numero
+                    else -> throw java.lang.IllegalArgumentException("Operação Inválida:")
+                }
+            }
+
+
         }
         binding.textResult.text = total.toString()
+
+
         return total
     }
 
+    fun deleteAll() {
+        lista.clear()
+        valorDisplay = ""
+        valorStringInput = ""
+        binding.textInfo.text = ""
+        binding.textResult.text = ""
+    }
 
+    fun deleteLast(){
 
-
-
-
-
+       valorStringInput = valorStringInput.dropLast(1)
+        valorDisplay = valorDisplay.dropLast(1)
+        binding.textInfo.text = valorDisplay
+    }
 
 }
